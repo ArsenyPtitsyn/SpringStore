b156aa7927f44aca54118679d48d251af3dcc8e8
senyabird <senyabird@yandex.ru>
2022-01-09 22:14:39 +0300

    renamed OrderService.java to OrdersService.java

--- homework1/src/main/java/com/geekbrains/spring/web/services/OrdersService.java
+++ homework1/src/main/java/com/geekbrains/spring/web/services/OrdersService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class OrderService {
+public class OrdersService {
     private final OrdersRepository ordersRepository;
 
 
