b969818cf6f4f180d05fb6b6edb00f1d597f5064
senyabird <senyabird@yandex.ru>
2022-01-26 22:47:51 +0300

    homework5.

---
+++ homework5/product-service/src/main/java/ru/geekbrains/spring/cloud/ProductsService.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.spring.cloud;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductsService {
+    private final ProductsRepository productsRepository;
+
+    public List<Product> findAll() {
+        return productsRepository.findAll();
+    }
+}
---
+++ homework5/product-service/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,10 @@
+create table products (
+    id          bigserial primary key,
+    title       varchar(255),
+    price       int
+);
+
+insert into products (title, price)
+values ('Milk', 100),
+       ('Bread', 80),
+       ('Cheese', 90);
