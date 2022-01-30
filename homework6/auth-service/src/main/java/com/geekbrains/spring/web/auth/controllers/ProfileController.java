d8393a4ef246a9ba54472fc84fafbfe884834cee
senyabird <senyabird@yandex.ru>
2022-01-30 17:03:29 +0300

    Probably completed.

--- homework6/auth-service/src/main/java/com/geekbrains/spring/web/auth/controllers/ProfileController.java
+++ homework6/auth-service/src/main/java/com/geekbrains/spring/web/auth/controllers/ProfileController.java
@@ -1,6 +1,6 @@
-package com.geekbrains.spring.web.core.controllers;
+package com.geekbrains.spring.web.auth.controllers;
 
-import com.geekbrains.spring.web.core.dto.ProfileDto;
+import com.geekbrains.spring.web.auth.dto.ProfileDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
